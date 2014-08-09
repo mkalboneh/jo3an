3e9a9b7c8f88c7a1af9b51ae3de3608f010bcead
hammamsamara <h.samara.74@gmail.com>
2014-08-09 16:12:57 +0300

    add polls

--- config/models.js
+++ config/models.js
@@ -18,5 +18,6 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
 
-  // connection: 'localDiskDb'
+  connection: 'mongo',
+  migrate: 'safe'
 };
